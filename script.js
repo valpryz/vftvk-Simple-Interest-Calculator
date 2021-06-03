fb70515b80b9366a7d6568c01fdc639b14fc213f
valpryz <valpryz@gmail.com>
2021-06-03 11:54:18 +0100

    project completed

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+let slider = document.querySelector("#rate");
+let sliderValue = document.querySelector(".value");
+
+slider.oninput = function () {
+  sliderValue.innerHTML = slider.value;
+};
+
+function compute() {
+  let amount = document.querySelector("#principal");
+  let years = document.querySelector("#years");
+  let result = document.querySelector("#result");
+  let compute =
+    Number(amount.value) * (Number(slider.value) / 100) * Number(years.value);
+
+  result.innerHTML = `If you deposit <mark>${amount.value}</mark>,<br />
+  at an interest rate of <mark>${slider.value}%</mark>. <br />
+  You will receive an amount of <mark>${compute}</mark>, <br />
+  in the year <mark>${2021 + Number(years.value)}</mark>`;
 }
-        
